c171cac5cd1b23979a26ab2b05e1ff0b1691c19f
Acid <acidburnmonkey@gmail.com>
2025-11-28 03:12:36 -0500

    new file:   .XCompose

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -176,6 +176,17 @@ local plugins = {
     -- harpoon
     {'ThePrimeagen/harpoon',lazy=false},
 
+    --MarkdownPreview
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && npm install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
+
 }
 
 local opts = {}
