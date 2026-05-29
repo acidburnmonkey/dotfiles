314b091ce706a748910b41fe4b888b5f861f09e1
Acid <acidburnmonkey@gmail.com>
2026-05-29 15:03:27 -0400

    monitor constrain

--- desktop/hypr/mods/keybinds.lua
+++ desktop/hypr/mods/keybinds.lua
@@ -9,7 +9,7 @@ local menu = "~/.config/rofi/rofimenu/launcher.sh"
 local apps = "~/.config/rofi/apps/apps.sh"
 local powermenu = "~/.config/rofi/rofipowermenu/powermenu.sh"
 local screenshot = 'grim -g "$(slurp)" ' .. os.getenv("HOME") .. "/screenshots/$(date +'%Y-%m-%d-%H%M%S').png"
-local mouseConstrain = "~/scripts/mouseConstrain.sh"
+local monitorConstrain = require("mods/mouseConstrain")
 
 -- App launchers
 hl.bind(mainMod .. " + R", hl.dsp.exec_cmd(terminal))
@@ -22,7 +22,7 @@ hl.bind(mainMod .. " + D", hl.dsp.exec_cmd(menu))
 hl.bind(mainMod .. " + S", hl.dsp.exec_cmd(apps))
 hl.bind("CTRL + ALT + L", hl.dsp.exec_cmd(powermenu))
 hl.bind(mainMod .. " + L", hl.dsp.exec_cmd("loginctl lock-session"))
-hl.bind(mainMod .. " + G", hl.dsp.exec_cmd(mouseConstrain))
+hl.bind(mainMod .. " + G", monitorConstrain)
 hl.bind(mainMod .. " + N", hl.dsp.exec_cmd("~/.config/quickshell/network/network_toggle.sh"))
 
 -- Special workspace (scratchpad)
