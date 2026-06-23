d7c238f698c77522ffd0ee6a0e420d6d9a20eb90
mal0 <acidburnmonkey@gmail.com>
2026-06-23 12:29:23 -0400

    modified:   Laptop-configs/hypr/modules/windows.lua

--- Laptop-configs/hypr/modules/windows.lua
+++ Laptop-configs/hypr/modules/windows.lua
@@ -10,8 +10,7 @@ hl.window_rule({ match = { class = "^(net.thunderbird.Thunderbird)$" }, workspac
 hl.window_rule({ match = { class = "^(org.gnome.Evince)$" }, workspace = "5" })
 hl.window_rule({ match = { class = "^(discord)$" }, workspace = "5" })
 hl.window_rule({ match = { title = "^(VSCodium)$" }, workspace = "4" })
-hl.window_rule({ match = { title = "^(RStudio)$" }, workspace = "4" })
-hl.window_rule({ match = { class = "^(jetbrains-idea-ce)$" }, workspace = "4" })
+hl.window_rule({ match = { title = "^(jetbrains-idea)$" }, workspace = "4" })
 hl.window_rule({ match = { class = "^(obsidian)$" }, workspace = "8" })
 hl.window_rule({ match = { title = "^(Jellyfin Media Player)$" }, workspace = "8" })
 hl.window_rule({ match = { class = "^(chrome-calendar.google.com__calendar_u_0_r-Profile_1)$" }, workspace = "8" })
@@ -22,6 +21,6 @@ hl.window_rule({ match = { class = "^(Virt-manager)$" }, workspace = "9" })
 hl.window_rule({ match = { class = ".*" }, suppress_event = "maximize" })
 hl.window_rule({ match = { title = "^(.*Rofi.*)$" }, stay_focused = true })
 hl.window_rule({
-  match = { class = "^(jetbrains-studio)$", title = "^win.*" },
-  no_initial_focus = true,
+	match = { class = "^(jetbrains-studio)$", title = "^win.*" },
+	no_initial_focus = true,
 })
